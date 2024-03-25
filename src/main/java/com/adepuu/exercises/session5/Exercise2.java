b0524111845e16fe3127d5b87d3cc3114803fca9
fikrim6 <fikrim0615@gmail.com>
2024-03-25 14:42:38 +0700

    no2 fikri

--- src/main/java/com/adepuu/exercises/session5/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise2.java
@@ -1,5 +1,7 @@
 package com.adepuu.exercises.session5;
 
+import java.util.Scanner;
+
 public class Exercise2 {
     /**
      * Java Array Program For Array Rotation
@@ -9,6 +11,21 @@ public class Exercise2 {
      * So, Final result: 3, 4, 5, 6, 7, 1, 2
      */
     public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Jumlah rotasi: ");
+        int rotasi = scanner.nextInt();
+        for (int i = 0; i < rotasi; i++) {
+            int temp = array[0];
+            for (int j = 0; j < array.length - 1; j++) {
+                array[j] = array[j + 1];
+            }
+            array[array.length - 1] = temp;
+        }
+        System.out.print("Output :");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
 
     }
 }
