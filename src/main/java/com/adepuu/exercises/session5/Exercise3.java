03401dee36b273cb18d954f9013b17f2c7b26624
fikrim6 <fikrim0615@gmail.com>
2024-03-25 15:24:52 +0700

    no3 fikri

--- src/main/java/com/adepuu/exercises/session5/Exercise3.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise3.java
@@ -1,5 +1,7 @@
 package com.adepuu.exercises.session5;
 
+import java.util.Scanner;
+
 public class Exercise3 {
     /**
      * Write a Java Program to Check if Array Contain Duplicates
@@ -17,6 +19,33 @@ public class Exercise3 {
      * Output: true
      */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Angka yang ingin dimasukkan : ");
+        int size = scanner.nextInt();
 
+        int[] array = new int[size];
+        System.out.println("Masukkan angka:");
+        for (int i = 0; i < size; i++) {
+            System.out.print("Angka " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+        }
+
+        boolean dupe = checkDuplicates(array);
+        if (dupe) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+        scanner.close();
+    }
+    public static boolean checkDuplicates(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
