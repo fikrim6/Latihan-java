29d1420166c6f5e067d28ba2162c7cf14bdaf1e5
fikrim6 <fikrim0615@gmail.com>
2024-03-25 15:25:36 +0700

    no3 fikri

--- src/main/java/com/adepuu/exercises/session5/Exercise3.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise3.java
@@ -5,15 +5,12 @@ import java.util.Scanner;
 public class Exercise3 {
     /**
      * Write a Java Program to Check if Array Contain Duplicates
-     *
      * Example 1:
      * Input: nums = [1,2,3,1]
      * Output: true
-     *
      * Example 2:
      * Input: nums = [1,2,3,4]
      * Output: false
-     *
      * Example 3:
      * Input: nums = [1,1,1,3,3,4,3,2,4,2]
      * Output: true
