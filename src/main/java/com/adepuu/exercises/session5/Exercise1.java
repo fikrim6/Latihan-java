1523870c9178874f6a71ebc1b7fc677074d11dc6
fikrim6 <fikrim0615@gmail.com>
2024-03-25 14:42:22 +0700

    no1 fikri revisi

--- src/main/java/com/adepuu/exercises/session5/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise1.java
@@ -17,26 +17,6 @@ public class Exercise1 {
             }
         }
         System.out.println("Output: " + largest);
-
-
-     // Rotation
-        System.out.println("Rotasi");
-        int[] array2 = {1, 2, 3, 4, 5, 6, 7};
-        Scanner scanner2 = new Scanner(System.in);
-        System.out.print("Jumlah rotasi: ");
-        int rotasi = scanner2.nextInt();
-        for (int i = 0; i < rotasi; i++) {
-            int temp = array2[0];
-            for (int j = 0; j < array2.length - 1; j++) {
-                array2[j] = array2[j + 1];
-            }
-            array2[array2.length - 1] = temp;
-        }
-        System.out.print("Output :");
-        for (int num : array2) {
-            System.out.print(num + " ");
-        }
         scanner.close();
-        scanner2.close();
     }
 }
