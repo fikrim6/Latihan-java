b0f1f143d54667104ab16170db8ecf9ce115c99f
fikrim6 <fikrim0615@gmail.com>
2024-03-25 15:46:45 +0700

    no4 fikri

--- src/main/java/com/adepuu/exercises/session5/Exercise4.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise4.java
@@ -1,5 +1,7 @@
 package com.adepuu.exercises.session5;
 
+import java.util.Scanner;
+
 public class Exercise4 {
     /**
      * Java Array Program to Remove Duplicate Elements From an Array
@@ -9,6 +11,47 @@ public class Exercise4 {
      * @param args
      */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan total angka yang ingin dimasukkan: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+        System.out.println("Masukkan angka");
+        for (int i = 0; i < size; i++) {
+            System.out.print("Angka " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+        }
+
+        int[] arrayb = removeDuplicates(array);
+        System.out.println("Output:");
+        for (int angka : arrayb) {
+            System.out.print(angka + " ");
+        }
+    }
+
+    public static int[] removeDuplicates(int[] array) {
+        int[] arrayb = new int[array.length];
+        int uniqueCount = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            boolean isDuplicate = false;
+            for (int j = 0; j < uniqueCount; j++) {
+                if (array[i] == arrayb[j]) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if (!isDuplicate) {
+                arrayb[uniqueCount] = array[i];
+                uniqueCount++;
+            }
+        }
+
+        int[] arrayc = new int[uniqueCount];
+        for (int i = 0; i < uniqueCount; i++) {
+            arrayc[i] = arrayb[i];
+        }
+        return arrayc;
 
     }
 }
